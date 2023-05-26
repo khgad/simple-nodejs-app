191cc3ceac51a2da23f3387fd2174df72bfc8fa3
khgad <khaled.626528@gmail.com>
2023-05-26 21:22:20 +0300

    develop simple nodejs app

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express(); 
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/iti', function(req, res) {
+    res.send('<h2>Cloud Platform Development</h2><h3>Intake: 43</h3><h4>Author: Khaled gad</h4>')
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
